482b7b237d8fc25280822df6afd4a9e08c27c8b9
Micael Santos <124316875+Micaelsantosdev@users.noreply.github.com>
2024-05-07 21:02:09 -0300

    Add files via upload

---
+++ desafios/controle-fluxo/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package DesafioControleFluxo.src;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
